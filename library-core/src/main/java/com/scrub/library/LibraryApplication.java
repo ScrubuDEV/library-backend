c8f77728693daa153495c672f2e128677c60ff3e
Kostas Kontos <kostas.kontos@vasscompany.com>
2025-02-23 20:30:36 +0200

    OP-002: Modified poms, added Book entity and basic crud endpoints.

--- library-core/src/main/java/com/scrub/library/LibraryApplication.java
+++ library-core/src/main/java/com/scrub/library/LibraryApplication.java
@@ -3,7 +3,7 @@ package com.scrub.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.scrub.web.controllers","com.scrub.library"})
 public class LibraryApplication {
 
 	public static void main(String[] args) {
